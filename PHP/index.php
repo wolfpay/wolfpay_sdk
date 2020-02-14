e0802b20755acf5d3b3d67a6edfbfae0bb5777b6
黃柏澄(風雨) <wartw@users.noreply.github.com>
2020-02-15 03:31:27 +0800

    Create index.php

---
+++ PHP/index.php
@@ -0,0 +1,36 @@
+<?php
+//引入支付核心类库
+include './Pay.class.php';
+
+//引入配置文件
+include './config.php';
+
+//实例化支付类
+$pay = new Pay($pid, $key, $api);
+
+//支付方式(all:全接口,alipay:支付宝,qqpay:QQ钱包,wxpay:微信支付)
+$type = 'all';
+
+//订单号
+$out_trade_no = time();
+
+//异步通知地址
+$notify_url = 'https://' . $_SERVER['HTTP_HOST'] . '/notify.php';
+
+//回调通知地址
+$return_url = 'https://' . $_SERVER['HTTP_HOST'] . '/return.php';
+
+//商品名称
+$name = '测试商品';
+
+//支付金额（保留小数点后两位）
+$money = '0.01';
+
+//站点名称
+$sitename = '测试支付';
+
+//发起支付
+$url = $pay->submit($type, $out_trade_no, $notify_url, $return_url, $name, $money, $sitename);
+
+//输出表单
+echo "<script>window.location.href='{$url}';</script>";
