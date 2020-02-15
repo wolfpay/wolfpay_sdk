9c335ef2ffca07ec773d73b894e31a842ecba818
黃柏澄(風雨) <wartw@users.noreply.github.com>
2020-02-15 23:54:10 +0800

    Update Pay.class.php

--- PHP/Pay.class.php
+++ PHP/Pay.class.php
@@ -56,6 +56,7 @@ class Pay {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
         $output = curl_exec($ch);
         curl_close($ch);
         return $output;
